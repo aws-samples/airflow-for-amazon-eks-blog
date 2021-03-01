5ff1ed838e9b733684c7b2d21d197fea34738a06
Elamaran Shanmugam <elamaras@amazon.com>
2021-03-01 12:18:02 -0500

    Re Changes

--- scripts/setup_infra.sh
+++ scripts/setup_infra.sh
@@ -11,16 +11,16 @@
 # - Environment variables AOK_AWS_REGION, AOK_EKS_CLUSTER_NAME should be set.
 
 
-printf "Step1. Deploy Kubernetes Cluster Autoscaler.\n"
+printf "Deploy Kubernetes Cluster Autoscaler.\n"
 
-printf "Associating OIDC provider with the EKS cluster....\n"
+printf "Associating OIDC provider with the EKS cluster...\n"
 
 eksctl utils associate-iam-oidc-provider \
    --region $AOK_AWS_REGION \
    --cluster $AOK_EKS_CLUSTER_NAME\
    --approve
-   
-printf "Creating an IAM policy document for cluster autoscaler....\n"
+
+printf "Creating an IAM policy document for cluster autoscaler...\n"
 cat << EOF > cluster-autoscaler-policy.json
 {
     "Version": "2012-10-17",
@@ -42,12 +42,12 @@ cat << EOF > cluster-autoscaler-policy.json
 }
 EOF
 
-printf "Creating the IAM policy....\n"
-aws iam create-policy \
+printf "Creating the IAM policy...\n"
+AmazonEKSClusterAutoscalerPolicy=(aws iam create-policy \
   --policy-name AmazonEKSClusterAutoscalerPolicy \
-  --policy-document file://cluster-autoscaler-policy.json
+  --policy-document file://cluster-autoscaler-policy.json)
 
-printf "Creating  service account for cluster autoscaler....\n"
+printf "Creating service account for cluster autoscaler...\n"
 eksctl create iamserviceaccount \
   --cluster=$AOK_EKS_CLUSTER_NAME \
   --namespace=kube-system \
@@ -57,11 +57,11 @@ eksctl create iamserviceaccount \
   --region $AOK_AWS_REGION \
   --approve
 
-printf "Adding the cluster autoscaler helm repo....\n"
+printf "Adding cluster autoscaler helm repo....\n"
 helm repo add autoscaler https://kubernetes.github.io/autoscaler
 helm repo update
 
-printf "Installing the cluster autoscaler....\n"
+printf "Installing cluster autoscaler....\n"
 helm install cluster-autoscaler \
   autoscaler/cluster-autoscaler \
   --namespace kube-system \
@@ -75,16 +75,16 @@ helm install cluster-autoscaler \
 
 
 
-printf "2. Deploy the EFS CSI driver and create EFS filesystem and Access Point.\n"
+printf "Deploy the EFS CSI driver and create EFS filesystem and Access Point.\n"
 
-printf "Deploying EFS Driver....\n"
+printf "Deploying EFS Driver...\n"
 helm repo add aws-efs-csi-driver https://kubernetes-sigs.github.io/aws-efs-csi-driver/
 helm repo update
 helm upgrade --install aws-efs-csi-driver \
   aws-efs-csi-driver/aws-efs-csi-driver \
   --namespace kube-system
 
-printf "Getting the VPC of the EKS cluster and its CIDR block....\n"
+printf "Getting the VPC of the EKS cluster and its CIDR block...\n"
 export AOK_VPC_ID=$(aws eks describe-cluster --name $AOK_EKS_CLUSTER_NAME \
   --region $AOK_AWS_REGION \
   --query "cluster.resourcesVpcConfig.vpcId" \
@@ -94,7 +94,7 @@ export AOK_CIDR_BLOCK=$(aws ec2 describe-vpcs --vpc-ids $AOK_VPC_ID \
   --region $AOK_AWS_REGION \
   --output text)
 
-printf "Creating a security group for EFS, and allow inbound NFS traffic (port 2049):....\n"
+printf "Creating a security group for EFS, and allow inbound NFS traffic (port 2049):...\n"
 export AOK_EFS_SG_ID=$(aws ec2 create-security-group \
   --region $AOK_AWS_REGION \
   --description Airflow-on-EKS \
@@ -110,7 +110,7 @@ aws ec2 authorize-security-group-ingress \
   --cidr $AOK_CIDR_BLOCK \
   --region $AOK_AWS_REGION
 
-printf "Creating an EFS file system....\n"
+printf "Creating an EFS file system...\n"
 export AOK_EFS_FS_ID=$(aws efs create-file-system \
   --creation-token Airflow-on-EKS \
   --performance-mode generalPurpose \
@@ -121,10 +121,10 @@ export AOK_EFS_FS_ID=$(aws efs create-file-system \
   --output text \
   --query "FileSystemId")
 
-printf "Waiting for 10 seconds....\n"
+printf "Waiting for 10 seconds...\n"
 sleep 10
 
-printf "Creating EFS mount targets in each subnet attached to on-demand nodes....\n"
+printf "Creating EFS mount targets in each subnet attached to on-demand nodes...\n"
 for subnet in $(aws eks describe-nodegroup \
   --cluster-name $AOK_EKS_CLUSTER_NAME \
   --nodegroup-name ng-on-demand \
@@ -138,7 +138,7 @@ do (aws efs create-mount-target \
   --region $AOK_AWS_REGION); \
 done
 
-printf "Creating an EFS access point....\n"
+printf "Creating an EFS access point...\n"
 export AOK_EFS_AP=$(aws efs create-access-point \
   --file-system-id $AOK_EFS_FS_ID \
   --posix-user Uid=1000,Gid=1000 \
@@ -147,27 +147,27 @@ export AOK_EFS_AP=$(aws efs create-access-point \
   --query 'AccessPointId' \
   --output text)
 
-printf "3. Deploy an Amazon RDS PostgreSQL database.\n"
+printf "Deploy an Amazon RDS PostgreSQL database.\n"
 
-printf "Obtaining the list of Private Subnets in Env variables....\n"
-AOK_PRIVATE_SUBNETS=$(aws eks describe-nodegroup \
+printf "Obtaining the list of Private Subnets in Env variables...\n"
+export AOK_PRIVATE_SUBNETS=$(aws eks describe-nodegroup \
   --cluster-name $AOK_EKS_CLUSTER_NAME \
   --nodegroup-name ng-on-demand \
   --region $AOK_AWS_REGION \
   --output text \
   --query "nodegroup.subnets" | awk -v OFS="," '{for(i=1;i<=NF;i++)if($i~/subnet/)$i="\"" $i "\"";$1=$1}1')
 
-printf "Creating a DB Subnet group....\n"
+printf "Creating a DB Subnet group...\n"
 aws rds create-db-subnet-group \
    --db-subnet-group-name airflow-postgres-subnet \
    --subnet-ids "[$AOK_PRIVATE_SUBNETS]" \
    --db-subnet-group-description "Subnet group for Postgres RDS" \
    --region $AOK_AWS_REGION
 
-printf "Creating the RDS Postgres Instance....\n"
+printf "Creating the RDS Postgres Instance...\n"
 aws rds create-db-instance \
   --db-instance-identifier airflow-postgres \
-  --db-instance-class db.t3.micro \
+  --db-instance-class db.m4.large \
   --db-name airflow \
   --db-subnet-group-name airflow-postgres-subnet \
   --engine postgres \
@@ -178,14 +178,14 @@ aws rds create-db-instance \
   --region $AOK_AWS_REGION
   
   
-printf "Creating RDS security group....\n"
-AOK_RDS_SG=$(aws rds describe-db-instances \
+printf "Creating RDS security group...\n"
+export AOK_RDS_SG=$(aws rds describe-db-instances \
    --db-instance-identifier airflow-postgres \
    --region $AOK_AWS_REGION \
    --query "DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId" \
    --output text)
 
-printf "Authorizing traffic....\n"
+printf "Authorizing traffic...\n"
 aws ec2 authorize-security-group-ingress \
   --group-id $AOK_RDS_SG \
   --cidr $AOK_CIDR_BLOCK \
@@ -193,7 +193,7 @@ aws ec2 authorize-security-group-ingress \
   --protocol tcp \
   --region $AOK_AWS_REGION
 
-printf "Waiting for 5 minutes....\n"
+printf "Waiting for 5 minutes...\n"
 sleep 300 
 
 printf "Checking if the RDS Instance is up ....\n"
